56185a3681509bfc901c1b9183629b1129e3d566
Angga Negara <angga@me.com>
2017-12-12 16:17:38 +0800

    visual update

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -9,20 +9,26 @@
                 </agile>
 				<h1 class="intro-title">FITNESS FOOD & MORE</h1>
 				<div class="intro-subtitle">
-					Clean eating, designed for optimal nutrition delivered to your home or office.<br>
-					100% Homemade & Fresh - Whole Foods - Organic - No Refined Sugar.<br>
-					All Motion Meals are available as Vegan / Vegetarian / Gluten-Free / Dairy-Free option upon request.
+					<p class="intro-big">Clean eating, designed for optimal nutrition delivered to your home or office.</p>
+					<p class="intro-big">100% Homemade & Fresh - Whole Foods - Organic - No Refined Sugar.</p>
+					<p class="intro-box">All Motion Meals are available as Vegan / Vegetarian / Gluten-Free / Dairy-Free option upon request.</p>
 				</div>
 			</div>
 		</div>
 		<div class="container">
 			<items/>
             <how-it-works level="2" :popup="false"></how-it-works>
+            <div class="text-xs-center checkout-box" v-if="cartState.length > 0">
+				<router-link to="/overview" class="button yellow big">
+					<b>checkout <i class="fa fa-fw fa-angle-right"></i></b>
+				</router-link>
+			</div>
 		</div>
 	</section>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 import Items from './Items.vue'
 import HowItWorks from './HowItWorks.vue'
 import Cart from './Cart.vue'
@@ -34,7 +40,12 @@ export default {
 		return {
 			dropdown: false
 		}
-	},
+    },
+    computed: {
+        ...mapState({
+            cartState: state => state.cart
+        })
+    },
 	methods: {
 		openDropdown() {
 			this.dropdown = ! this.dropdown
